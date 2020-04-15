7d26b8cd926ef7f30fef4c3aac4df75552f95ed2
Adrian Wolny <adrian.wolny@iwr.uni-heidelberg.de>
2020-04-15 12:38:38 +0200

    Improve logging + tests

--- test/outliers/test_outlier.py
+++ test/outliers/test_outlier.py
@@ -13,3 +13,25 @@ class TestOutliers(unittest.TestCase):
         plate_name = '20200406_164555_328'
         img_name = 'WellA01_PointA01_0008_ChannelDAPI,WF_GFP,TRITC_Seq0008'
         assert outliers.is_outlier(plate_name, img_name)
+
+    def test_outlier_number(self):
+        outliers = Outliers(self._global_path)
+        plate_count = {}
+        total_outlier_count = 0
+        total_count = 0
+
+        for plate_name, state in outliers.outliers.items():
+            count = 0
+            outlier_count = 0
+            for img_file in state:
+                if outliers.is_outlier(plate_name, img_file):
+                    outlier_count += 1
+                    total_outlier_count += 1
+                count += 1
+                total_count += 1
+
+            plate_count[plate_name] = outlier_count / count
+
+        for k, v in plate_count.items():
+            print(f'{k}: {v}')
+        print(f'\nTotal outlier count: {total_outlier_count / total_count}')
---
+++ test/util/test_logging.py
@@ -0,0 +1,42 @@
+import tempfile
+import unittest
+import os
+
+from batchlib.util.logging import get_logger, add_file_handler
+
+
+class TestOutliers(unittest.TestCase):
+    def test_propagation(self):
+        with tempfile.TemporaryDirectory() as work_dir:
+            parent = get_logger('Workflow')
+            add_file_handler(parent, work_dir, 'workflow1')
+
+            # create child logger
+            child = get_logger('Workflow.Job')
+            child.warning('test log')
+
+            # assert that log event was passed to the file handler
+            log_file = os.path.join(work_dir, 'workflow1.log')
+            with open(log_file) as f:
+                num_lines = sum(1 for _ in f)
+
+            assert num_lines == 1
+
+    def test_log_rollover(self):
+        with tempfile.TemporaryDirectory() as work_dir:
+            logger = get_logger('Workflow')
+            workflow_name = 'workflow1'
+
+            add_file_handler(logger, work_dir, workflow_name)
+            logger.warning('test1')
+            # create new file handler
+            add_file_handler(logger, work_dir, workflow_name)
+            logger.warning('test2')
+            # create yet another file handler
+            add_file_handler(logger, work_dir, workflow_name)
+            logger.warning('test3')
+
+            log_path = os.path.join(work_dir, workflow_name + '.log')
+            expected_log_files = [log_path, log_path + '.1', log_path + '.2']
+
+            assert all(os.path.exists(fp) for fp in expected_log_files)
