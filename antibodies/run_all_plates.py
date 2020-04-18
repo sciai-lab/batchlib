8c17d89bcd018d2643a491d13b6ed48f5c45f968
Your Name <you@example.com>
2020-04-18 13:26:44 +0000

    add pixelwise workflow to run_all_plates.py

--- antibodies/run_all_plates.py
+++ antibodies/run_all_plates.py
@@ -2,8 +2,9 @@
 
 from glob import glob
 import os
-from run_all_workflows import run_all_workflows
+import sys
 from instance_analysis_workflow2 import run_instance_analysis2, parse_instance_config2
+from pixel_analysis_workflow1 import run_pixel_analysis1, parse_pixel_config1
 
 
 def run_instance2(in_folder, use_unique_output_folder):
@@ -13,22 +14,26 @@ def run_instance2(in_folder, use_unique_output_folder):
     run_instance_analysis2(config)
 
 
-def run_all_plates(with_corrected=True):
-    in_folder = '/home/covid19/data/covid-data-vibor'
-    folders = glob(in_folder + '/*')
+def run_pixel1(in_folder, use_unique_output_folder):
+    config = parse_pixel_config1()
+    config.input_folder = in_folder
+    config.use_unique_output_folder = use_unique_output_folder
+    run_pixel_analysis1(config)
+
+
+def run_plates(folders):
 
     for folder in folders:
-        if 'test' in folder:
-            continue
         if not os.path.isdir(folder):
             continue
         if 'channel_mapping.json' not in os.listdir(folder):
             print(f'\nSkipping {folder} because it is missing channel_mapping.json\n')
             continue
         # run_all_workflows(folder)
-        # TODO don't hard-code to this workflow
+        # TODO don't hard-code to these workflows
         try:
             run_instance2(folder, use_unique_output_folder=False)
+            run_pixel1(folder, use_unique_output_folder=False)
         except Exception as e:
             print(f'\nException while evaluating folder {folder}.')
             print(e)
@@ -36,4 +41,20 @@ def run_all_plates(with_corrected=True):
     print('all plates processed')
 
 
-run_all_plates()
+if __name__ == '__main__':
+    # hack to parse all folders given as first arguments
+    folders = []
+    while len(sys.argv) > 1 and not sys.argv[1].startswith('-'):
+        folders.append(sys.argv[1])
+        del sys.argv[1]
+    if len(folders) == 0:
+        # default to all folders in covid-data-vibor
+        in_folder = '/home/covid19/data/covid-data-vibor'
+        folders = glob(in_folder + '/*')
+        print(sys.argv)
+    print('Processing these plates:')
+    [print(folder) for folder in folders]
+    print()
+    print(f'Args being passed to workflows: {sys.argv}\n')
+
+    run_plates(folders)
