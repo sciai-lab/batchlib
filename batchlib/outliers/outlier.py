103e51638206428187fcc25fbc5c2b6e36a037c8
Adrian Wolny <adrian.wolny@iwr.uni-heidelberg.de>
2020-04-28 14:18:57 +0200

    Clean up outlier detection

--- batchlib/outliers/outlier.py
+++ batchlib/outliers/outlier.py
@@ -18,16 +18,20 @@ def get_outlier_predicate(config):
     if hasattr(config, 'plate_name') and config.plate_name is not None:
         plate_name = config.plate_name
         if isinstance(plate_name, list):
+            assert len(plate_name) == 1, \
+                f'Only a single plate name is allowed, but {len(plate_name)} were given to the outlier predicate'
             plate_name = plate_name[0]
         logger.info(f'Using plate name: {plate_name}')
     else:
         logger.info(f"Trying to parse 'plate_name' from the input folder: {config.input_folder}")
         plate_name = plate_name_from_input_folder(config.input_folder)
         if plate_name is not None:
-            logger.info(f"'plate_name' found: {plate_name}")
+            logger.info(f"plate_name found: {plate_name}")
         else:
-            # return default predicate, i.e. treat all images as non-outliers
-            return lambda im: False
+            logger.info(f"Cannot parse plate_name. Outlier detection will be skipped")
+            # no plate name was given and it cannot be parsed from the config.input_folder
+            # so we skip outlier detection, i.e. assign outlier: None to each of the images
+            return lambda im: None
 
     return OutlierPredicate(root_table_dir=outliers_dir, plate_name=plate_name)
 
@@ -43,29 +47,63 @@ def plate_name_from_input_folder(input_folder):
 
 
 class OutlierPredicate:
-    def __init__(self, root_table_dir, plate_name=None):
+    def __init__(self, root_table_dir, plate_name):
         """
-        Parses CSV files stored in 'root_table_dir', where each CSV corresponds to a given plate, and stores
-        the results in a dictionary of the form {PLATE_NAME: TAGGER_STATE}, where TAGGER_STATE is a dict
-        containing all images in a given PLATE_NAME together with their labels: (0 - accepted, 1 - outlier, -1 - skipped).
+        Iterates over the CSV files stored in 'root_table_dir', where each CSV stores outlier tagging to a given plate,
+        finds a CSV corresponding to a given 'plate_name' and loads the outlier tag for each image in the plate.
+        Labels are:
+            0 - accepted image
+            1 - outlier image
+            -1 - skipped (not tagged) image
 
-        If plate_name is provided only image names from this plate will be considered for outlier detection.
+        If a given 'plate_name' cannot be found in the 'root_table_dir' a warning is raised.
         """
 
-        self.outliers = {}
+        assert plate_name is not None, 'plate_name cannot be None'
+
+        self.outlier_tags = None
 
         # parse outliers
         for csv_file in glob.glob(os.path.join(root_table_dir, '*.csv')):
             assert '_tagger_state.csv' in csv_file
             pn = os.path.split(csv_file)[1]
             pn = pn[:pn.find('_tagger')]
-            self.outliers[pn] = self._load_state(csv_file)
+            if pn == plate_name:
+                self.outlier_tags = self._load_state(csv_file)
 
-        self.plate_name = plate_name
-        if plate_name is not None:
-            assert isinstance(plate_name, str)
-            assert plate_name in self.outliers, \
-                f'Plate name: {pn} not found. Loaded plates: {list(self.outliers.keys())}'
+        if self.outlier_tags is None:
+            # plate_name could not be found in root_table_dir
+            logger.warning(f'Plate name: {plate_name} could not be found in {root_table_dir}. '
+                           f'Outlier detection will be skipped. Make sure to put the outlier CSV file inside the {root_table_dir}.')
+
+    def __call__(self, img_file):
+        """
+        Check if a given image (img_file) was marked as an outlier.
+
+        Returns:
+            True if the img_file was marked as an outlier
+            False if the img_file was marked as a valid image
+            None if the img_file was skipped during tagging, or outliers are not available for a given plate (no outlier CSV file)
+        """
+
+        # take only the file
+        img_file = os.path.split(img_file)[1]
+        # skip file extension if any
+        img_file = os.path.splitext(img_file)[0]
+
+        if self.outlier_tags is None:
+            # outliers info not available
+            return None
+
+        label = self.outlier_tags[img_file]
+        if label == 1:
+            return True
+        elif label == 0:
+            return False
+        elif label == -1:
+            return None
+        else:
+            raise ValueError(f'Unsupported outlier label value: {label}')
 
     @staticmethod
     def _load_state(csv_file):
@@ -81,27 +119,3 @@ class OutlierPredicate:
                 # update state
                 state[filename] = label
         return state
-
-    def __call__(self, img_file):
-        return self.is_outlier(self.plate_name, img_file)
-
-    def is_outlier(self, plate_name, img_file):
-        """
-        Returns True if a given image (img_file) from a given plate (plate_name) is an outlier, False otherwise.
-        WARN: currently we treat everything not labeled as 0 to be an outlier (i.e. images labeled as skipped -1
-        are counted as outliers).
-        """
-        assert plate_name in self.outliers, f'Well name: {plate_name} not found. Loaded plates: {list(self.outliers.keys())}'
-        plate_state = self.outliers[plate_name]
-
-        # take only the file
-        img_file = os.path.split(img_file)[1]
-        # skip file extension if any
-        img_file = os.path.splitext(img_file)[0]
-        if not img_file in plate_state:
-            logger.warning(f'Cannot find image file: {img_file} in plate: {plate_name}')
-            return False
-
-        label = plate_state[img_file]
-
-        return label != 0
