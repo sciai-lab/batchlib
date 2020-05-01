32fc8e0b8c687bd8b80d3bac8af34bc20877c1ec
Constantin Pape <c.pape@gmx.net>
2020-05-01 22:30:27 +0200

    Update proofreading scripts

--- proofreading/export_infected_cells_groundtruth.py
+++ proofreading/export_infected_cells_groundtruth.py
@@ -1,20 +1,34 @@
 import argparse
+import numpy as np
 import h5py
+import nifty.ground_truth as ngt
+
+
+def check(raw, seg):
+    import napari
+    with napari.gui_qt():
+        viewer = napari.Viewer()
+        viewer.add_image(raw)
+        viewer.add_labels(seg)
 
 
 def export_from_bigcat(in_path, out_path, ignore_label):
     with h5py.File(in_path, 'r') as f:
         raw = f['volumes/raw'][:]
-        seg = f['volumes/labels/merged_ids'][0, :, :].astype('int64')
+        seg = f['volumes/labels/merged_ids'][0].astype('int64')
+        mask = f['volumes/infected_mask'][:]
+    assert seg.shape == mask.shape
 
-    # need to bring labels back to [0, 1, 2]
-    # 0 - not infected label
-    # 1 - infected label
-    # 2 - background label
-    seg -= seg.min()
+    seg_ids = np.unique(seg)
+    overlaps = ngt.overlap(seg, mask)
+    overlaps = {sid: overlaps.overlapArrays(sid, True)[0][0] for sid in seg_ids}
 
-    # remap the ignore label
-    seg[seg == 2] = ignore_label
+    map_values = {0: 0, 1: 1, 2: ignore_label}
+    for seg_id in seg_ids:
+        seg[seg == seg_id] = map_values[overlaps[seg_id]]
+
+    assert set(np.unique(seg)) == {0, 1, ignore_label}
+    # check(raw, seg)
 
     with h5py.File(out_path, 'a') as f:
         f.create_dataset('raw', data=raw, compression='gzip')
