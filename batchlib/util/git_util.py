56556f748692c2033059b37f19abc17eebeffd7c
Adrian Wolny <adrian.wolny@iwr.uni-heidelberg.de>
2020-05-04 13:24:14 +0200

    Add logging to git_utils

--- batchlib/util/git_util.py
+++ batchlib/util/git_util.py
@@ -1,9 +1,14 @@
 from subprocess import check_output
 
+from batchlib.util import get_logger
+
+logger = get_logger('Workflow.GitUtil')
+
 
 def get_commit_id():
     try:
         commit_id = check_output(['git', 'rev-parse', 'HEAD']).decode('utf-8').rstrip('\n')
-    except Exception:
+    except Exception as e:
+        logger.warning(f'Cannot get git commit SHA: {e}')
         commit_id = None
     return commit_id
