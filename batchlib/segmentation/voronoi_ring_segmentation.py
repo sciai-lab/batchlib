fdc2a83cc1f204d43820470d95ba26e03751baea
Roman Remme <roman.remme@gmx.de>
2020-05-07 01:04:39 +0200

    add ErodeSegmentation

--- batchlib/segmentation/voronoi_ring_segmentation.py
+++ batchlib/segmentation/voronoi_ring_segmentation.py
@@ -5,7 +5,7 @@ from tqdm.auto import tqdm
 import numpy as np
 
 from batchlib.base import BatchJobOnContainer
-from batchlib.util import open_file
+from batchlib.util import open_file, seg_to_edges
 
 
 class VoronoiRingSegmentation(BatchJobOnContainer):
@@ -42,3 +42,26 @@ class VoronoiRingSegmentation(BatchJobOnContainer):
             labels = self.segment_image(in_path)
             with open_file(out_path, 'a') as f:
                 self.write_image(f, self.output_key, labels)
+
+
+class ErodeSegmentation(BatchJobOnContainer):
+    """
+    """
+    def __init__(self, radius, bg_label=0, **super_kwargs):
+        super().__init__(input_ndim=2, output_ndim=2, **super_kwargs)
+        self.radius = radius
+        if bg_label != 0:
+            raise NotImplementedError
+
+    def process_image(self, in_path, out_path):
+        with open_file(in_path, 'r') as f:
+            seg = self.read_image(f, self.input_key)
+        boundaries_and_bg = seg_to_edges(seg)
+        seg[morph.dilation(boundaries_and_bg, morph.disk(self.radius))] = 0
+        with open_file(out_path, 'a') as f:
+            self.write_image(f, self.output_key, seg)
+
+    def run(self, input_files, output_files):
+        for in_path, out_path in tqdm(zip(input_files, output_files), total=len(input_files),
+                                      desc='computing eroded segmentations'):
+            self.process_image(in_path, out_path)
