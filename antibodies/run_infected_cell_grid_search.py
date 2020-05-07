80c196fd045263deb39c12c3b1869f21aa60d7dc
Roman Remme <roman.remme@gmx.de>
2020-05-07 02:16:25 +0200

    grid search: update params

--- antibodies/run_infected_cell_grid_search.py
+++ antibodies/run_infected_cell_grid_search.py
@@ -5,40 +5,43 @@ import numpy as np
 if __name__ == '__main__':
 
     class SubParamRanges:
-        ks_for_topk = [10, 30, 40, 45, 50, 55, 60, 100, 200]
+        ks_for_topk = [30, 40, 45, 50, 55, 60, 100]
         quantiles = [0.8, 0.9, 0.95, 0.98, 0.99, 0.995]
-        ring_widths = [20]  #[10, 50]  #[20, 50]
-
-    class SearchSpace:
-        segmentation_key = ['cell_segmentation'] + [f'voronoi_ring_segmentation{r}' for r in SubParamRanges.ring_widths]
-        ignore_nuclei = [True, False]
-        split_statistic = [f'top{k}' for k in SubParamRanges.ks_for_topk] + [f'quantile{q}' for q in
-                                                                             SubParamRanges.quantiles]
-        infected_threshold = np.arange(0, 20, 0.05)  #np.arange(0, 4000, 50)
-        marker_denoise_radii = [0, 5, 10]  # TODO use those
-
-    # For debugging
-    class SubParamRanges:
-        ks_for_topk = [50]
-        quantiles = [0.9]
-        ring_widths = []
+        ring_widths = []  #[10, 50]  #[20, 50]
         segmentation_erode_radii = [1, 3, 5]
 
     class SearchSpace:
         segmentation_key = ['cell_segmentation'] + \
                            [f'eroded_cell_segmentation{r}' for r in SubParamRanges.segmentation_erode_radii] + \
                            [f'voronoi_ring_segmentation{r}' for r in SubParamRanges.ring_widths]
-
         ignore_nuclei = [True, False]
         split_statistic = [f'top{k}' for k in SubParamRanges.ks_for_topk] + [f'quantile{q}' for q in
                                                                              SubParamRanges.quantiles]
-        infected_threshold = np.arange(0, 20, 0.5)  #np.arange(0, 4000, 50)
+        infected_threshold = np.arange(0, 20, 0.1)  #np.arange(0, 4000, 50)
         marker_denoise_radii = [0, 5, 10]
+
+    # # For debugging
+    # class SubParamRanges:
+    #     ks_for_topk = [50]
+    #     quantiles = [0.9]
+    #     ring_widths = []
+    #     segmentation_erode_radii = [1, 3, 5]
+    #
+    # class SearchSpace:
+    #     segmentation_key = ['cell_segmentation'] + \
+    #                        [f'eroded_cell_segmentation{r}' for r in SubParamRanges.segmentation_erode_radii] + \
+    #                        [f'voronoi_ring_segmentation{r}' for r in SubParamRanges.ring_widths]
+    #
+    #     ignore_nuclei = [True, False]
+    #     split_statistic = [f'top{k}' for k in SubParamRanges.ks_for_topk] + [f'quantile{q}' for q in
+    #                                                                          SubParamRanges.quantiles]
+    #     infected_threshold = np.arange(0, 20, 0.5)  #np.arange(0, 4000, 50)
+    #     marker_denoise_radii = [0, 5, 10]
 
     class config:  # TODO make an argument parser for this
         ann_dir = '/home_sdc/rremme_tmp/src/antibodies-nuclei/groundtruth'
         data_dir = '/home_sdc/rremme_tmp/DatasetsHCIHome/antibodies/covid-data-vibor/'
-        out_dir = '/home_sdc/rremme_tmp/DatasetsHCIHome/antibodies/grid_search/debug_2'
+        out_dir = '/home_sdc/rremme_tmp/DatasetsHCIHome/antibodies/grid_search/run_20200507_00'
         misc_folder = '/home_sdc/rremme_tmp/src/batchlib/misc'
 
         mask_key = 'mask'
