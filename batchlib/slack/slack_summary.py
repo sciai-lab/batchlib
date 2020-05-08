3c4974e45924f903b980c8c6eaa6e2708bcf2694
Constantin Pape <c.pape@gmx.net>
2020-05-08 19:40:15 +0200

    Update slack writer

--- batchlib/slack/slack_summary.py
+++ batchlib/slack/slack_summary.py
@@ -9,12 +9,11 @@ from slack.errors import SlackApiError
 from ..base import BatchJobOnContainer
 from ..util import get_logger, open_file
 
-# TODO double check with Roman
 DEFAULT_PLOT_PATTERNS = ('*ratio_of_q0.5_of_means*per-well.png',
                          '*robust_z_score_means*per-well.png')
 
-# DEFAULT_SLACK_CHANNEL = '#latest-results'
-DEFAULT_SLACK_CHANNEL = '#random'
+DEFAULT_SLACK_CHANNEL = '#latest-results'
+# DEFAULT_SLACK_CHANNEL = '#random'
 
 logger = get_logger('Workflow.BatchJob.SlackSummaryWriter')
 
@@ -64,30 +63,46 @@ class SlackSummaryWriter(BatchJobOnContainer):
     def copy_table(self, input_file, out_path):
         with open_file(input_file, 'r') as f:
             columns, table = self.read_table(f, self.summary_table_key)
+            param_cols, param_table = self.read_table(f, self.parameter_table_key)
+
+        # copy the summary table to csv
         table = pd.DataFrame(table, columns=columns)
         table.to_csv(out_path, index=False)
 
-    # TODO need to put more info in the message:
-    # - what's the background value ?
-    # - is it computed or was it pre-set ?
-    def compile_message(self, out_folder, plate_name, runtime):
+        # get the parameter dict
+        params = {name: val for name, val in zip(param_cols, param_table.squeeze())}
+        return params
+
+    def compile_message(self, out_folder, plate_name, runtime,
+                        use_fixed_background, background_type, background_values):
         msg_path = os.path.join(self.out_folder, 'message.txt')
+
         with open(msg_path, 'w') as f:
-            f.write("Results for plate %s:\n" % plate_name)
-            # TODO write where the results are
-            # f.write()
+            f.write(f"Results for plate {plate_name}:\n")
+            f.write(f"Files are stored in {self.folder}\n")
+
             if runtime is not None:
                 # TODO format to hours
-                f.write("Ran in %f s \n" % runtime)
+                f.write(f"Computation ran in {runtime} s\n")
 
-    # TODO
-    # - need to format the message nicer using block kit, see
-    #   https://slack.dev/python-slackclient/basic_usage.html
-    # - need to post the actual message
-    # - check if we can get to format .csv properly or try posting as excel
+            if use_fixed_background:
+                f.write(f"The background was fixed to {background_values}\n")
+            else:
+                f.write(f"The background was computed on the {background_type}\n")
+
     def post_slack(self, out_folder):
         msg_sent_path = os.path.join(self.out_folder, 'message_sent.txt')
-        upload_patterns = ('*.png', '*.csv')
+
+        # load the messages
+        msg_path = os.path.join(self.out_folder, 'message.txt')
+        with open(msg_path) as f:
+            message = f.read()
+
+        image_paths = glob(os.path.join(out_folder, '*.png'))
+        image_paths.sort()
+        table_path = glob(os.path.join(out_folder, '*.csv'))
+        assert len(table_path) == 1
+        table_path = table_path[0]
 
         # get slack client and connect to the workspace
         client = WebClient(token=self.slack_token)
@@ -95,21 +110,23 @@ class SlackSummaryWriter(BatchJobOnContainer):
         # post summary to slack
         try:
             logger.info(f"{self.name}: successfully connected to workspace")
+            # TODO would be nice to integrate the file upload in here as well,
+            # but I don't know how
+            message_blocks = [
+                {
+                    "type": "section",
+                    "text": {"text": message, "type": "plain_text"}
+                }
+            ]
             response = client.chat_postMessage(channel=self.slack_channel,
-                                               text="Hello world!")
+                                               blocks=message_blocks)
             logger.debug(f"{self.name}: posted {response} to {self.slack_channel}")
 
-            upload_files = []
-            for pattern in upload_patterns:
-                to_upload = glob(os.path.join(out_folder, pattern))
-                to_upload.sort()
-                upload_files.extend(to_upload)
-
-            logger.info(f"{self.name}: posting the files {upload_files}")
-
-            for ff in upload_files:
-                response = client.files_upload(channels=self.slack_channel, file=ff)
+            for im_path in image_paths:
+                response = client.files_upload(channels=self.slack_channel, file=im_path)
                 logger.debug(f"{self.name}: posted {response} to {self.slack_channel}")
+
+            # TODO upload the table in proper format (maybe excel?)
 
             # we just write this file if the slack message was sent
             with open(msg_sent_path, 'w'):
@@ -117,6 +134,23 @@ class SlackSummaryWriter(BatchJobOnContainer):
 
         except SlackApiError as e:
             logger.info(f"{self.name}: connection failed with {e.response['error']}")
+
+    def parse_bg_params(self, params):
+        print(params)
+        background_type = params.get('background_type', None)
+        use_fixed_background = True if params['fixed_background'] == 'True' else False
+
+        if background_type is None and (not use_fixed_background):
+            raise RuntimeError("Invalid background parameter")
+
+        if use_fixed_background:
+            background_values = {k.lstrip('background_'): v for k, v in params.items() if k.startswith('background_')}
+        else:
+            background_values = {}
+
+        return {'use_fixed_background': use_fixed_background,
+                'background_type': background_type,
+                'background_values': background_values}
 
     def run(self, input_files, output_files, runtime=None):
         if len(input_files) != 1 or len(output_files) != 1:
@@ -132,12 +166,13 @@ class SlackSummaryWriter(BatchJobOnContainer):
         plot_folder = os.path.join(self.folder, 'plots')
         self.copy_plots(plot_folder, out_folder)
 
-        # copy the well table as csv to the summary folder
+        # copy the well table as csv to the summary folder and load the analysis parameter
         table_out_path = os.path.join(out_folder, '%s_summary.csv' % plate_name)
-        self.copy_table(input_files[0], table_out_path)
+        params = self.copy_table(input_files[0], table_out_path)
 
         # compile the slack message
-        self.compile_message(out_folder, plate_name, runtime)
+        bg_params = self.parse_bg_params(params)
+        self.compile_message(out_folder, plate_name, runtime, **bg_params)
 
         # post to slack with the slack bot
         if self.slack_token is None:
