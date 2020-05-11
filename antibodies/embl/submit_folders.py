e98f1783f8fa071c67354e46f47cbd0e20b61ece
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2020-05-11 13:36:35 +0200

    Add tischi mode

--- antibodies/embl/submit_folders.py
+++ antibodies/embl/submit_folders.py
@@ -10,20 +10,26 @@ ALT_BG_PLATES = ('plate6rep2_wp_20200507_131032_010',
                  'titration_plate_20200403_154849')
 
 
-def get_bg_config_file(folder):
+def get_bg_config_file(folder, tischi_mode):
     plate_name = os.path.split(folder)[1]
-    if plate_name in ALT_BG_PLATES:
-        return './configs/cell_analysis_alt_bg.conf'
+    if tischi_mode:
+        if plate_name in ALT_BG_PLATES:
+            return './configs/tischi_alt_bg.conf'
+        else:
+            return './configs/tischi_bg.conf'
     else:
-        return './configs/cell_analysis_bg.conf'
+        if plate_name in ALT_BG_PLATES:
+            return './configs/cell_analysis_alt_bg.conf'
+        else:
+            return './configs/cell_analysis_bg.conf'
 
 
-def submit_folders(folder_list, config_file=None, fixed_background=False):
+def submit_folders(folder_list, config_file=None, fixed_background=False, tischi_mode=False):
     assert all(os.path.exists(folder) for folder in folder_list), str(folder_list)
     for folder in folder_list:
 
         if fixed_background:
-            this_config_file = get_bg_config_file(folder)
+            this_config_file = get_bg_config_file(folder, tischi_mode)
         else:
             assert config_file is not None
             this_config_file = config_file
@@ -44,6 +50,7 @@ if __name__ == '__main__':
     parser.add_argument('input_file', type=str, help="json file with list of folders to be processed")
     parser.add_argument('--config_file', type=str, default='', help='config file to be used')
     parser.add_argument('--fixed_background', type=int, default=0, help='run computation with fixed background')
+    parser.add_argument('--tischi_mode', type=int, default=0, help='recompute stuff for tischi')
     parser.add_argument('--write_to_slack', type=int, default=0, help='post results to slack')
 
     args = parser.parse_args()
@@ -52,6 +59,10 @@ if __name__ == '__main__':
     config_file = args.config_file
     fixed_background = bool(args.fixed_background)
     write_to_slack = bool(args.write_to_slack)
+    tischi_mode = bool(args.tischi_mode)
+
+    if tischi_mode:
+        assert fixed_background
 
     if config_file != '' and fixed_background:
         raise ValueError("Invalid parameter combination")
@@ -62,4 +73,4 @@ if __name__ == '__main__':
     with open(in_file, 'r') as f:
         folder_list = json.load(f)
 
-    submit_folders(folder_list, config_file, fixed_background)
+    submit_folders(folder_list, config_file, fixed_background, tischi_mode)
