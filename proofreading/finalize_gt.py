1a208d0176b2dec5d1dbd2cce757457f46a4f1c1
Constantin Pape <c.pape@gmx.net>
2020-05-13 16:12:36 +0200

    Continue finalize gt script

--- proofreading/finalize_gt.py
+++ proofreading/finalize_gt.py
@@ -1,11 +1,28 @@
 import os
 import napari
+import nifty.ground_truth as ngt
+import nifty.tools as nt
+import numpy as np
+import vigra
 
 from batchlib.util import read_image, write_image, write_table, open_file
 
 
+# BE AWARE, there are two types of images for the infected mask!
+# this is for the one based on the segmentation image,
+# need to also implement for the other case
 def get_infected_labels(seg, infected_mask):
-    pass
+    unique_seg_ids = np.unique(seg)
+    overlap = ngt.overlap(seg, infected_mask)
+
+    infected_overlaps = np.array([overlap.overlapArrays(seg_id, True)[0][0]
+                                  for seg_id in unique_seg_ids], dtype='uint64')
+
+    new_infected_mask = nt.take(infected_overlaps, seg.astype('uint64'))
+
+    cols = ['label_id', 'infected_label']
+    table = np.concatenate([unique_seg_ids[:, None], infected_overlaps[:, None].astype('float32')], axis=1)
+    return new_infected_mask, cols, table
 
 
 def finalize_gt_volume(data_path, seg_gt_path, infected_gt_path, out_path,
@@ -14,12 +31,18 @@ def finalize_gt_volume(data_path, seg_gt_path, infected_gt_path, out_path,
     # TODO load all the raw data
     with open_file(data_path, 'r') as f:
         serum_igg = read_image(f, 'serum_IgG')
+        marker = read_image(f, 'marker')
+        nuclei = read_image(f, 'nuclei')
 
+        serum_igg_raw = read_image(f, 'serum_IgG_raw')
+        marker_raw = read_image(f, 'marker_raw')
+        nuclei_raw = read_image(f, 'nuclei_raw')
     shape = serum_igg.shape
 
     # TODO load the segmentation gt
     with open_file(seg_gt_path, 'r') as f:
         seg = f['cells'][:]
+        seg = vigra.analysis.relabelConsecutive(seg, start_label=1, keep_zeros=True)[0]
     assert seg.shape == shape
 
     with open_file(infected_gt_path, 'r') as f:
@@ -27,7 +50,17 @@ def finalize_gt_volume(data_path, seg_gt_path, infected_gt_path, out_path,
     assert infected_mask.shape == shape
 
     # map infected gt nucleus labels to cell segmentation
-    infected_labels_cols, infected_labels_table = get_infected_labels(seg, infected_mask)
+    (infected_mask,
+     infected_labels_cols,
+     infected_labels_table) = get_infected_labels(seg, infected_mask)
+
+    # with napari.gui_qt():
+    #     viewer = napari.Viewer()
+    #     viewer.add_image(serum_igg, name='serum')
+    #     viewer.add_image(marker, name='marker')
+    #     viewer.add_labels(seg, name='segmentation')
+    #     viewer.add_labels(infected_mask, name='infected-mask')
+    # quit()
 
     if check:
         inspect()
@@ -35,7 +68,20 @@ def finalize_gt_volume(data_path, seg_gt_path, infected_gt_path, out_path,
         # TODO write everything
         with open_file(out_path, 'a') as f:
             write_image(f, 'serum_IgG', serum_igg)
+            write_image(f, 'marker', marker)
+            write_image(f, 'nuclei', nuclei)
+
+            write_image(f, 'serum_IgG', serum_igg_raw)
+            write_image(f, 'marker_raw', marker_raw)
+            write_image(f, 'nuclei_raw', nuclei_raw)
+
+            write_image(f, 'cell_segmentation', seg)
+            write_image(f, 'infected_mask', infected_mask)
+
             write_table(f, 'infected_cell_labels', infected_labels_cols, infected_labels_table)
+
+            f.attrs['plate'] = plate_name
+            f.attrs['image'] = base_name
 
 
 def inspect():
@@ -70,8 +116,22 @@ def finalize_all(gt_root_path, gt_folder, data_root, check=False):
             ii += 1
 
 
+def test_single_gt():
+    seg_path = '/home/pape/Work/covid/antibodies-nuclei/groundtruth/segmentation/20200420_164920_764_IgG/WellD11_PointD11_0000_ChannelDAPI,WF_GFP,TRITC,WF_Cy5,DIA_Seq0270_segmentation_done.h5'
+    infected_path = '/home/pape/Work/covid/antibodies-nuclei/groundtruth/infection/20200420_164920_764_IgG/WellD11_PointD11_0000_ChannelDAPI,WF_GFP,TRITC,WF_Cy5,DIA_Seq0270_infected.h5'
+    data_path = '/home/pape/Work/covid/antibodies-nuclei/groundtruth/data/WellD11_PointD11_0000_ChannelDAPI,WF_GFP,TRITC,WF_Cy5,DIA_Seq0270.h5'
+
+    out_path = './test.h5'
+    base_name = os.path.splitext(os.path.split(data_path)[1])[0]
+    plate_name = '20200420_164920_764'
+
+    finalize_gt_volume(data_path, seg_path, infected_path, out_path,
+                       plate_name, base_name, False)
+
+
 if __name__ == '__main__':
-    gt_root_path = '/home/pape/Work/covid/antibodies-nuclei/groundtruth'
-    gt_folder = '/home/pape/Work/covid/antibodies-nuclei/groundtruth/merged'
-    data_root = ''
-    finalize_all(gt_root_path, gt_folder, data_root)
+    test_single_gt()
+    # gt_root_path = '/home/pape/Work/covid/antibodies-nuclei/groundtruth'
+    # gt_folder = '/home/pape/Work/covid/antibodies-nuclei/groundtruth/merged'
+    # data_root = ''
+    # finalize_all(gt_root_path, gt_folder, data_root)
