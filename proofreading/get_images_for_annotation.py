f4b51356f01153a83c57c8ecedb636790decdd33
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2020-05-26 13:59:04 +0200

    Finished image annotation prep script

--- proofreading/get_images_for_annotation.py
+++ proofreading/get_images_for_annotation.py
@@ -1,8 +1,21 @@
-import pandas as pd
 import os
 
+import h5py
+import pandas as pd
+from batchlib.util import read_image, write_image
 
-def make_image_list(initial_table, root):
+
+def export_for_annotation(in_path, out_path):
+    keys = ['serum_IgG', 'nuclei', 'marker', 'cell_segmentation']
+
+    with h5py.File(in_path, 'r') as f, h5py.File(out_path, 'a') as f_out:
+        for key in keys:
+            im = read_image(f, key)
+            write_image(f_out, key, im)
+
+
+def make_image_list(initial_table, root, root_out):
+    os.makedirs(root_out, exist_ok=True)
     initial_table = pd.read_excel(initial_table)
 
     plate_names = initial_table['plate'].values
@@ -20,11 +33,17 @@ def make_image_list(initial_table, root):
     paths = list(set(paths))
     paths.sort()
 
-    for p in paths:
-        print(p)
+    for in_path in paths:
+        assert os.path.exists(in_path), in_path
+        folder, file_name = os.path.split(in_path)
+        plate_name = os.path.split(folder)[1]
+        out_name = f'{plate_name}_{file_name}'
+        out_path = os.path.join(root_out, out_name)
+        export_for_annotation(in_path, out_path)
 
 
 if __name__ == '__main__':
     initial_table = './Stacks2proofread.xlsx'
-    root = ''
-    make_image_list(initial_table, root)
+    root = '/g/kreshuk/data/covid/data-processed'
+    root_out = '/g/kreshuk/data/covid/for_annotation/round1'
+    make_image_list(initial_table, root, root_out)
