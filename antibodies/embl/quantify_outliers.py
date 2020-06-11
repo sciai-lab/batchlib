2f6dcb01e2a4ade5fc75a8d6bf050f2c0895bfb7
Constantin Pape <c.pape@gmx.net>
2020-06-11 21:47:45 +0200

    Update outlier quantification

--- antibodies/embl/quantify_outliers.py
+++ antibodies/embl/quantify_outliers.py
@@ -1,4 +1,5 @@
 import os
+from glob import glob
 import h5py
 
 from batchlib.util import read_table
@@ -16,25 +17,61 @@ def quantify_well_or_image(folder, name):
     table_path = os.path.join(folder, f'{plate_name}_table.hdf5')
     with h5py.File(table_path, 'r') as f:
         cols, tab = read_table(f, f'images/outliers')
-    print(cols, tab)
     n = len(tab)
     outliers = tab[:, cols.index('is_outlier')] == 1
     n_outliers = outliers.sum()
+
     outlier_type = tab[:, cols.index('outlier_type')]
-    print(len([otype for otype in outlier_type if 'manual: -1' in otype]), '/', n)
-    return n, n_outliers
+    n_manual = len([otype for otype in outlier_type if 'manual: 1' in otype])
+
+    return n, n_outliers, n_manual
 
 
 def quantify_well_or_image_outlier(name):
 
     plates = get_manuscript_plates()
+    n_samples, n_outliers, n_manual = 0, 0, 0
+    for plate in plates:
+        ns, no, nm = quantify_well_or_image(plate, name)
+        n_samples += ns
+        n_outliers += no
+        n_manual += nm
+
+    print("Outliers per", name, ":")
+    print(n_outliers, "/", n_samples, "=", float(n_outliers) / n_samples)
+
+    n_heuristic = n_outliers - n_manual
+    print("Heuristic outliers:")
+    print(n_heuristic, "/", n_samples, "=", float(n_heuristic) / n_samples)
+
+    print("Manual outliers:")
+    print(n_manual, "/", n_samples, "=", float(n_manual) / n_samples)
+
+
+def cell_outliers_per_plate(plate):
+    image_files = glob(os.path.join(plate, '*.h5'))
+    outlier_tab_name = 'cell_segmentation_serum_IgG/serum_IgG_outliers'
+
+    n, n_outliers = 0, 0
+    for imf in image_files:
+        with h5py.File(imf, 'r') as f:
+            cols, tab = read_table(f, outlier_tab_name)
+    n = len(tab)
+    outliers = tab[:, cols.index('is_outlier')] == 1
+    n_outliers = outliers.sum()
+    return n, n_outliers
+
+
+def quantify_cell_outliers():
+    plates = get_manuscript_plates()
+
     n_samples, n_outliers = 0, 0
     for plate in plates:
-        ns, no = quantify_well_or_image(plate, name)
+        ns, no = cell_outliers_per_plate(plate)
         n_samples += ns
         n_outliers += no
 
-    print("Outliers per", name, ":")
+    print("Outliers per cell :")
     print(n_outliers, "/", n_samples, "=", float(n_outliers) / n_samples)
 
 
@@ -42,3 +79,5 @@ if __name__ == '__main__':
     quantify_well_or_image_outlier('wells')
     print()
     quantify_well_or_image_outlier('images')
+    print()
+    quantify_cell_outliers()
