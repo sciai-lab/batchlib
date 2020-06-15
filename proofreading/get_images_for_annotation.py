a6d93a27a7f9462ff0e3fd19a9436ede6e5cafe0
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2020-06-15 12:38:17 +0200

    Update image generation scripts

--- proofreading/get_images_for_annotation.py
+++ proofreading/get_images_for_annotation.py
@@ -2,6 +2,7 @@ import argparse
 import os
 
 import h5py
+import numpy as np
 import pandas as pd
 from batchlib.util import read_image, write_image
 
@@ -15,20 +16,25 @@ def export_for_annotation(in_path, out_path):
             write_image(f_out, key, im)
 
 
-def make_image_list(initial_table, root, root_out):
+def make_image_list(table, root, root_out):
     os.makedirs(root_out, exist_ok=True)
-    initial_table = pd.read_excel(initial_table)
+    table = pd.read_excel(table)
 
-    plate_names = initial_table['plate'].values
-    file_names = initial_table['file'].values
-    status = initial_table['infected'].values
+    plate_names = table['plate'].values
+    file_names = table['file'].values
+    if 'infected' in table:
+        status = table['infected'].values
+    else:
+        status = np.zeros(len(table))
 
     paths = []
     for plate_name, name, stat in zip(plate_names, file_names, status):
         if stat == 1.:
             continue
         plate_name = plate_name.replace('_IgG', '').replace('_IgA', '')
-        path = os.path.join(root, plate_name, name + '.h5')
+        path = os.path.join(root, plate_name, name)
+        if not path.endswith('.h5'):
+            path += '.h5'
         paths.append(path)
 
     paths = list(set(paths))
@@ -50,4 +56,4 @@ if __name__ == '__main__':
     parser.add_argument('--root_out', default='/g/kreshuk/data/covid/for_annotation/round1')
 
     args = parser.parse_args()
-    make_image_list(args.initial_table, args.root, args.root_out)
+    make_image_list(args.table, args.root, args.root_out)
--- proofreading/make_annotation_table2.py
+++ proofreading/make_annotation_table2.py
@@ -1,10 +1,13 @@
+import json
 import os
 from glob import glob
 
 import h5py
+import numpy as np
 import pandas as pd
 
-from batchlib.util import read_table
+from batchlib.util import read_table, image_name_to_well_name
+from tqdm import tqdm
 
 manuscript_plates = [
     "20200417_132123_311",
@@ -23,10 +26,14 @@ manuscript_plates = [
 
 
 def score_images(plate_folder, old_names):
-    images = glob(os.path.join(plate_folder, '*.h5'))
-    im_list = {}
-
     plate_name = os.path.split(plate_folder)[1]
+
+    cache_path = f'./plate_stats/{plate_name}.json'
+    if os.path.exists(cache_path):
+        with open(cache_path) as f:
+            return json.load(f)
+
+    images = glob(os.path.join(plate_folder, '*.h5'))
     table_path = os.path.join(plate_folder, f'{plate_name}_table.hdf5')
 
     with h5py.File(table_path, 'r') as f:
@@ -34,19 +41,107 @@ def score_images(plate_folder, old_names):
         well_cols, well_tab = read_table(f, 'wells/default')
 
         im_names = im_tab[:, im_cols.index('image_name')]
-        print(im_names)
-
         im_outliers = {name: iso for name, iso in zip(im_names,
                                                       im_tab[:, im_cols.index('IgG_is_outlier')])}
 
+        well_names = well_tab[:, well_cols.index('well_name')]
+        well_outliers = {name: iso for name, iso in zip(well_names,
+                                                        well_tab[:, well_cols.index('IgG_is_outlier')])}
+
+    im_names = []
+    ratios = []
+
+    tab_name = 'cell_classification/cell_segmentation/marker_tophat'
     for im in images:
-        im_name = os.path.split(im)
+        im_name = os.path.splitext(os.path.split(im)[1])[0]
         # check for outliers
+        if im_outliers[im_name] == 1:
+            continue
+        well_name = image_name_to_well_name(im_name)
+        if well_outliers[well_name] == 1:
+            continue
+        res_name = f'{plate_name}_{im_name}.h5'
+        if res_name in old_names:
+            continue
+
+        with h5py.File(im, 'r') as f:
+            cols, tab = read_table(f, tab_name)
+
+        # compute the infected to non infected ratio
+        n_infected = tab[:, cols.index('is_infected')].sum()
+        n_control = tab[:, cols.index('is_control')].sum()
+
+        if n_infected == 0 or n_control == 0:
+            continue
+
+        ratio = abs(1. - n_infected / float(n_control))
+        ratios.append(ratio)
+        im_names.append(im_name)
+
+    # sort by the ratio
+    im_names = np.array(im_names)
+    ratios = np.array(ratios)
+
+    ratio_sorted = np.argsort(ratios)
+    im_names = im_names[ratio_sorted].tolist()
+
+    with open(cache_path, 'w') as f:
+        json.dump(im_names, f)
+
+    return im_names
 
 
-def make_second_annotation_table():
+def select_images(images, n, out_table):
+    im_id = 0
+    plate_id = 0
+
+    plate_names = list(images.keys())
+    n_plates = len(plate_names)
+
+    for _ in range(n):
+        plate_name = plate_names[plate_id]
+        im_name = images[plate_name][im_id]
+        if im_name.endswith('.h5.h5'):
+            im_name = im_name[:-3]
+        out_table.append([plate_name, im_name])
+
+        plate_id += 1
+        if plate_id == n_plates:
+            plate_id = 0
+            im_id += 1
+
+    return out_table
+
+
+def make_second_annotation_table(n_new, n_old):
     root = '/g/kreshuk/data/covid/data-processed'
+
+    old_table = pd.read_excel('./Stacks2proofread.xlsx')
+    old_plates = old_table['plate'].values
+    old_file_names = old_table['file'].values
+    old_names = [f'{plate}_{name}' for plate, name in zip(old_plates, old_file_names)]
+
+    images = {}
+    for plate in tqdm(manuscript_plates):
+        plate_folder = os.path.join(root, plate)
+        ims = score_images(plate_folder, old_names)
+        images[plate] = ims
+
+    out_table = []
+    out_table = select_images(images, n_new, out_table)
+
+    old_images = {}
+    for plate, im_name in zip(old_plates, old_file_names):
+        if plate in old_images:
+            old_images[plate].append(im_name)
+        else:
+            old_images[plate] = [im_name]
+
+    out_table = select_images(old_images, n_old, out_table)
+
+    df = pd.DataFrame(out_table, columns=['plate', 'file'])
+    df.to_excel('./Stacks2proofread_round2.xlsx', index=False)
 
 
 if __name__ == '__main__':
-    pass
+    make_second_annotation_table(50, 10)
